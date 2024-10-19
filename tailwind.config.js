1d6fecdabce875fc069253aebd1470ff4674c237
joonjeong <joonjeong@users.noreply.github.com>
2024-10-19 10:48:09 +0000

    deploy: joonjeong/private@00a2c502ec72571c89ffcdc2ec19aa91a6869c3e

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/**/*.md",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
